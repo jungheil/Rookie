536fdca73c09dd3405ca08b4301cd7e885b09d36
jungheil <jungheilai@gmail.com>
2021-04-10 15:10:20 +0800

    更改协议

--- src/usart.cpp
+++ src/usart.cpp
@@ -160,13 +160,14 @@ int Usart::UsartSend(uint16_t* data)
 {
     int len = 0;
     unsigned char buff[30]={};
-    buff[0]=0x00;
+    buff[0]=0x01;
     buff[1]=(data[0]&0xFF00)>>8;
     buff[2]=data[0]&0x00FF;
     buff[3]=(data[1]&0xFF00)>>8;
     buff[4]=data[1]&0x00FF;
+    buff[5]=0x00FF;
     //TODO:Pheyeon-change the type of 'buff' and 'data' to char*   (finished)
-    AppendCRC16CheckSum(buff,kTotalSendLength);
+//    AppendCRC16CheckSum(buff,kTotalSendLength);
     len = write(Usart::serial_fd_, buff,kTotalSendLength );//实际写入的长度
 //    cout<<(int)data[0]<<","<<(int)data[1]<<endl;
 //    cout<<"l:"<<len<<"  buff:"<<((buff[1]<<8)|buff[2]) <<" "<<((buff[3]<<8)|buff[4])<<endl;
